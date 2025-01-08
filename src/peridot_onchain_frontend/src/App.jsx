a24cd20d5a91ab98bac98c626638375421f227b4
ranaufalmuha <ifal.muha@gmail.com>
2025-01-08 07:20:52 +0700

    Initial commit.

---
+++ src/peridot_onchain_frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { peridot_onchain_backend } from 'declarations/peridot_onchain_backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    peridot_onchain_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
