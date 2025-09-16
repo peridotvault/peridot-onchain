2e6d15e06f15a603590ec790e0148950d1f99c67
ranaufalmuha <ifal.muha@gmail.com>
2025-09-16 08:24:14 +0800

    update AI Chat UI responsive and Landing page Version download

--- src/peridot_onchain_frontend/src/pages/ai/AiSectionWelcome.jsx
+++ src/peridot_onchain_frontend/src/pages/ai/AiSectionWelcome.jsx
@@ -41,25 +41,25 @@ export const AiSectionWelcome = () => {
     }
 
     async function onKeyDown(e) {
-        if (e.key === "Enter" && !loading) { 
+        if (e.key === "Enter" && !loading) {
             e.preventDefault();
             await send(input);
         }
     }
 
     return (
-        <section className="w-full h-screen p-4 flex items-center justify-evenly relative bg-background_secondary">
+        <section className="w-full h-screen p-4 flex items-center justify-evenly max-md:flex-col duration-300 relative bg-background_secondary">
             <div>
                 <div>
-                    <div className='mb-24'>
-                        <p className='text-6xl font-medium'>Meet Our Intelligent AI</p>
+                    <div className=''>
+                        <p className='text-4xl font-medium'>Meet Our Intelligent AI</p>
                     </div>
                     <div>
-                        <p className='text-10xl text-accent_primary font-extrabold'>Peri</p>
+                        <p className='text-8xl text-accent_primary font-extrabold'>Peri</p>
                     </div>
                 </div>
             </div>
-            <div className='rounded-2xl border border-white/10 w-[768px] max-h-[768px] p-8 flex flex-col gap-8'>
+            <div className='rounded-2xl border border-white/10 w-1/2 max-md:w-full max-w-[768px] max-h-[768px] h-2/3 max-md:h-1/2 p-8 flex flex-col gap-8'>
                 {/* Header */}
                 <div className="flex flex-col items-center gap-4 text-center">
                     <h1 className="text-4xl font-bold max-md:text-3xl">How can I help you today?</h1>
